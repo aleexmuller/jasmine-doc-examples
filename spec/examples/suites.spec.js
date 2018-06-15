917608a4bbd48d56e08aa88a2ef530debfd01f41
Alex Muller de Jesus Sousa <a_lexmiller@hotmail.com>
2018-06-15 18:08:30 -0300

    adionando exemplo documentação para suites

---
+++ spec/examples/suites.spec.js
@@ -0,0 +1,11 @@
+/**
+ * Suites
+ * 
+ * A função describe é utilizada para agrupar specs relacionados. Esta função espera dois 
+ * parametros, o primeiro é uma string que é utilizada para nomear a coleção e o segundo parametro 
+ * é uma função anônima que será usada para invocar os testes propriamente dito.
+ * 
+ */
+describe("Suite", function() {
+  
+});
